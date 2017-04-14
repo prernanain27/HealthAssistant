703b62de96bc76e362152ec267790dc1ae6cdefc
manikhanuja <mani.khanuja@pb.com>
2017-04-14 01:59:26 -0700

    UI Enhancements

--- app/src/main/java/example/healthassistant/ViewMedicine.java
+++ app/src/main/java/example/healthassistant/ViewMedicine.java
@@ -1,32 +1,95 @@
 package example.healthassistant;
 
 import android.app.Activity;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
 import android.os.Bundle;
+//import android.support.v7.widget.Card;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.util.TypedValue;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ViewMedicine extends Activity {
 
-
+    private Context mContext;
+    private RelativeLayout mRelativeLayout;
     ListView medicineListView;
+    private SQLiteDatabase mDb;
+    SQLiteOpenHelper db;
+
+    ViewPrescription viewPrescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_medicine);
-
+        CardView[] cardViews = new CardView[20];
         medicineListView = (ListView) findViewById(R.id.medicineList);
-        Bundle imageInfo = getIntent().getExtras();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, imageInfo.getStringArray("medicineData"));
+        Bundle prescription = getIntent().getExtras();
+        String prescriptionName = prescription.getString("prescriptionName");
+        cardViews = getMedicineData(prescriptionName);
+        ArrayAdapter<CardView> simpleAdapter = new ArrayAdapter<CardView>(this, R.layout.medicine_item_cv, cardViews);
+        medicineListView.setAdapter(simpleAdapter);
+        medicineListView.getChildCount();
+    }
 
-        medicineListView.setAdapter(adapter);
+
+    public CardView[] getMedicineData(String prescriptionName) {
+        setContentView(R.layout.medicine_item_cv);
+        Log.d("ViewMedicine","Entered Get Medicine Data Method");
+        String whereClause = DbContract.DbEntryPrescription.COLUMN_PRESCRIPTION_NAME + "=?";
+        TextView tv = (TextView) findViewById(R.id.list_item_med_tv) ;
+        TextView tv1 = (TextView) findViewById(R.id.list_item_med_tv_heading) ;
+        String[] whereArgs = {prescriptionName};
+        CardView[] cardViewArrayList = new CardView[10];
+        mContext = getApplicationContext();
+        db =  new DbHelper(getApplicationContext());
+        mDb = db.getWritableDatabase();
+        Cursor cursor = null;
+        int i = 0;
+        try {
+            cursor = mDb.query(DbContract.DbEntryPrescription.TABLE_NAME, viewPrescription.ALL_COLUMNS, whereClause, whereArgs, null, null, null);
+            int indexMedName = cursor.getColumnIndex(DbContract.DbEntryPrescription.COLUMN_MED_NAME);
+            int indexMedTime = cursor.getColumnIndex(DbContract.DbEntryPrescription.COLUMN_MED_TIME);
+            int indexMedDose = cursor.getColumnIndex(DbContract.DbEntryPrescription.COLUMN_MED_DOSE);
+            int indexMedType = cursor.getColumnIndex(DbContract.DbEntryPrescription.COLUMN_MED_TYPE);
+            Log.d("GetMedicineData","MedicineName Index");
+            CardView card = (CardView) findViewById(R.id.card_view_medItem);
+            if (cursor.getCount() > 0) {
+
+                while (cursor.moveToNext()) {
+                    tv1.setText("Medicine Name:" + "\n" + "Medicine Time:" + "\n" +
+                            "Medicine Dose:" + "\n" + "Medicine Type:");
+                    tv.setText(cursor.getString(indexMedName) + "\n" + cursor.getString(indexMedTime) + "\n" +
+                           cursor.getString(indexMedDose) + "\n" + cursor.getString(indexMedType));
+                    cardViewArrayList[i] = card;
+                    i++;
+
+                    Log.d("GetMedicineData"," Inside Cursor Loop MedicineName Index: " + cursor.getString(indexMedName));
+
+                }
+            }
+        } catch (Exception e) {
+            Log.d("ViewMedicine", "Get Medicine Data method: Exception Raised with a value of " + e);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        db.close();
+        return cardViewArrayList;
     }
 
 
--- app/src/main/java/example/healthassistant/ViewPrescription.java
+++ app/src/main/java/example/healthassistant/ViewPrescription.java
@@ -1,21 +1,27 @@
 package example.healthassistant;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.Layout;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ViewPrescription extends AppCompatActivity {
-
     private SQLiteDatabase mDb;
     SQLiteOpenHelper db;
     ListView prescriptionListView;
@@ -26,7 +32,7 @@ public class ViewPrescription extends AppCompatActivity {
             "Medicine Time"
 
     };
-    private static final String[] ALL_COLUMNS = {
+    public static final String[] ALL_COLUMNS = {
 
             DbContract.DbEntryPrescription.COLUMN_ID,
             DbContract.DbEntryPrescription.COLUMN_PRESCRIPTION_NAME,
@@ -55,8 +61,8 @@ public class ViewPrescription extends AppCompatActivity {
                 c.moveToPosition(position);
                 String selectedFromList = c.getString(1);
                 Log.d("ViewPresciption", "Selected Item position " + position + "Selected Item: " + selectedFromList);
-                medicineData = getMedicineData(selectedFromList);
-                viewMedicine.putExtra("medicineData", medicineData);
+                //medicineData = getMedicineData(selectedFromList);
+               // viewMedicine.putExtra("medicineData", medicineData);
                 //Log.d("ViewPrescription", "Selected Prescription: " + medicineData[1] + "\n" + medicineData[2]);
                 viewMedicine.putExtra("prescriptionName", selectedFromList);
                 startActivity(viewMedicine);
@@ -78,12 +84,15 @@ public class ViewPrescription extends AppCompatActivity {
         }
         return  cursor;
     }
-    public String[] getMedicineData(String prescriptionName) {
+
+   /* public String[] getMedicineData(String prescriptionName) {
         Log.d("ViewPrescription","Entered Get Medicine Data Method");
         String whereClause = DbContract.DbEntryPrescription.COLUMN_PRESCRIPTION_NAME + "=?";
         String[] whereArgs = {prescriptionName};
+        mContext = getApplicationContext();
         mDb = db.getWritableDatabase();
         Cursor cursor = null;
+        int i = 0;
         try {
             cursor = mDb.query(DbContract.DbEntryPrescription.TABLE_NAME, ALL_COLUMNS, whereClause, whereArgs, null, null, null);
             int indexMedName = cursor.getColumnIndex(DbContract.DbEntryPrescription.COLUMN_MED_NAME);
@@ -93,12 +102,49 @@ public class ViewPrescription extends AppCompatActivity {
             Log.d("GetMedicineData","MedicineName Index");
             if (cursor.getCount() > 0) {
                 while (cursor.moveToNext()) {
+                    CardView card = new CardView(mContext);
+
+                    // Set the CardView layoutParams
+                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                            RelativeLayout.LayoutParams.WRAP_CONTENT,
+                            RelativeLayout.LayoutParams.WRAP_CONTENT
+                    );
+                    card.setLayoutParams(params);
+
+                    // Set CardView corner radius
+                    card.setRadius(9);
+
+                    // Set cardView content padding
+                    card.setContentPadding(15, 15, 15, 15);
+
+                    // Set a background color for CardView
+                    card.setCardBackgroundColor(Color.parseColor("#FFC6D6C3"));
+
+                    // Set the CardView maximum elevation
+                    card.setMaxCardElevation(15);
+
+                    // Set CardView elevation
+                    card.setCardElevation(9);
+
+                    // Initialize a new TextView to put in CardView
+                    TextView tv = new TextView(mContext);
+                    tv.setLayoutParams(params);
+                    tv.setText(cursor.getString(indexMedName) + "\n" + cursor.getString(indexMedTime) + "\n" +
+                            cursor.getString(indexMedDose) + "\n" + cursor.getString(indexMedType));
+                    tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+                    tv.setTextColor(Color.RED);
+
+                    // Put the TextView in CardView
+                    card.addView(tv);
+
+                    // Finally, add the CardView in root layout
+                    mRelativeLayout.addView(card);
                     Log.d("GetMedicineData"," Inside Cursor Loop MedicineName Index: ");
-                    medicineData[0] = cursor.getString(indexMedName);
-                    medicineData[1] = cursor.getString(indexMedTime);
-                    medicineData[2] = cursor.getString(indexMedDose);
-                    medicineData[3] = cursor.getString(indexMedType);
-                    Log.d("GetMedicineData", "Get Medicine Data Method: " + medicineData[0] + "\n "+ medicineData[2]);
+                    //medicineData[i] = cursor.getString(indexMedName);
+                   // medicineData[i+1] = cursor.getString(indexMedTime);
+                    //medicineData[i+2] = cursor.getString(indexMedDose);
+                    //medicineData[i+3] = cursor.getString(indexMedType);
+                   // Log.d("GetMedicineData", "Get Medicine Data Method: " + medicineData[0] + "\n "+ medicineData[2]);
                 }
             }
         } catch (Exception e) {
@@ -110,7 +156,8 @@ public class ViewPrescription extends AppCompatActivity {
         }
         db.close();
         return medicineData;
-    }
+    } */
+
     private void populateListView() {
         try {
                 Cursor cursor = getAllRows();
