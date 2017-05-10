beb6575ed235a2687e64c2edf5af3b2285ba9cbc
prernanain27 <prernanain27@gmail.com>
2017-05-10 01:30:46 -0700

    04/02/2017

--- app/src/main/java/example/healthassistant/Activities/NewUserData.java
+++ app/src/main/java/example/healthassistant/Activities/NewUserData.java
@@ -2,16 +2,16 @@ package example.healthassistant.Activities;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
-import android.support.v7.app.AlertDialog;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import java.lang.String;
 
 import example.healthassistant.DbContract;
 import example.healthassistant.DbHelper;
@@ -23,10 +23,12 @@ public class NewUserData extends AppCompatActivity {
     private EditText confirmPassword;
     private Button createUser;
     private SQLiteDatabase mDb;
+    SQLiteOpenHelper db;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user_data);
         email = (EditText)findViewById(R.id.signUpEmail);
@@ -39,16 +41,18 @@ public class NewUserData extends AppCompatActivity {
                 if(email.getText().toString()!=""){
                     if(!password.getText().toString().equals("")&&!confirmPassword.getText().toString().equals("")){
                         if(password.getText().toString().equals(confirmPassword.getText().toString())){
-                        //    String[] projection = {DbContract.DbEntry.COLUMN_EMAIL, DbContract.DbEntry.COLUMN_PASSWORD};
-                        //   Cursor data = mDb.query(DbContract.DbEntry.TABLE_NAME,projection, DbContract.DbEntry.COLUMN_EMAIL+ " = "+ email.getText().toString(),
-                          //          null,null,null,null,null);
-                         //   if(data.getCount()==0){
-                            addData(email.getText().toString(),password.getText().toString());
-                            Intent i = new Intent(NewUserData.this, AddPHRActivity.class);
-                            startActivity(i);
+                            db = new DbHelper(getApplicationContext());
+                            mDb = db.getReadableDatabase();
+                          String[] projection = {DbContract.DbEntry.COLUMN_EMAIL, DbContract.DbEntry.COLUMN_PASSWORD};
+                           Cursor data = mDb.query(DbContract.DbEntry.TABLE_NAME,projection, DbContract.DbEntry.COLUMN_EMAIL+ " = ?", new String[]{ email.getText().toString()}, null,null,null,null);
+                            if(!(data.getCount()>0)) {
+                                addData(email.getText().toString(), password.getText().toString());
+                                Intent i = new Intent(NewUserData.this, AddPHRActivity.class);
+                                startActivity(i);
+                            }
 
-                        //    else
-                        //        Toast.makeText(NewUserData.this, "Use other email address. This one already registered",Toast.LENGTH_SHORT).show();
+                            else
+                               Toast.makeText(NewUserData.this, "Use other email address. This one already registered",Toast.LENGTH_SHORT).show();
 
                         }
                         else
@@ -58,16 +62,8 @@ public class NewUserData extends AppCompatActivity {
                 }
                 else
                     Toast.makeText(NewUserData.this, "Enter valid Email Address",Toast.LENGTH_SHORT).show();
-
-                if (!email.getText().toString().trim().matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+")) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(NewUserData.this);
-                    builder.setMessage("Invalid Email Address")
-                            .setTitle("Error")
-                            .setPositiveButton(android.R.string.ok, null);
-                    AlertDialog dialog = builder.create();
-                    dialog.show();
-                }
             }
+
         });
     }
     public void addData(String email, String password) {
@@ -87,6 +83,7 @@ public class NewUserData extends AppCompatActivity {
         catch (SQLiteException ex){
             String s = ex.getMessage();
         }
+
     }
 
 
